5873cc74cd99469138e7cefec987a8f1020fc99b
Unknown <lelucasma@gmail.com>
2019-04-02 10:45:24 +0200

    initialisation index

---
+++ header.php
@@ -0,0 +1,5 @@
+<img src="images/nantzaah_blanc.svg"/>
+<a href=""><p>Accueil</p></a>
+<a href=""><p>Alternatives</p></a>
+<a href=""><p>Info-Trafic</p></a>
+<img src="images/icone_profil.png"/>
--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-Bonjour :)
-
-12.12 HA Corbiere
+<!DOCTYPE html> 
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="description" content="Aucune idée"/> 
+    <title>NANTZAAH</title>
+    <link rel="stylesheet" src="/css/style.css"/>
+</head>
+<body>
+    <header>
+        <?php require 'header.php';?>
+    </header>
+</body>
+</html>
